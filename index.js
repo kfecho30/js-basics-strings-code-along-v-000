50c41de38cd88067bb359c24abbff8e78610dedc
Kelsey Fecho <kfecho30@gmail.com>
2018-04-24 16:15:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
-let currentUser = "Kelsey"
-let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
-let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase + "!"
-let shortGreeting = "Welcome to Flatbook, " +
+let currentUser = "Kelsey";
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase + "!";
+let shortGreeting = "Welcome to Flatbook, ";
