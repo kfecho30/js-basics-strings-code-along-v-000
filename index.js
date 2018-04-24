5cccc4e6a02c1332c57505fcf4e6bb69b7ca5a47
Kelsey Fecho <kfecho30@gmail.com>
2018-04-24 16:16:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 let currentUser = "Kelsey";
 let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
 let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase + "!";
-let shortGreeting = "Welcome to Flatbook, ";
+let shortGreeting = "Welcome, ";
