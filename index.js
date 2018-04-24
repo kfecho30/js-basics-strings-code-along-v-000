c05f3ff22b0bcde722b86e2779380b9fb5d982a2
Kelsey Fecho <kfecho30@gmail.com>
2018-04-24 16:18:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
-let currentUser = "Kelsey";
-let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+const currentUser = "Kelsey";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase + "!";
 let shortGreeting = "Welcome, "+currentUser+"!";
